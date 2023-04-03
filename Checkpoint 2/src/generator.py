91ef5ca2da698c49f51bd6c0df18463331dc704a
Hugo Novais <a96267@alunos.uminho.pt>
2023-04-03 15:59:09 +0100

    começo do checkpoint 2

---
+++ Checkpoint 2/src/generator.py
@@ -0,0 +1,454 @@
+import sys
+import math
+
+def generateSphere(radius, slice, stack, outputFile):
+    triangles = []
+
+    slices = int(slice)
+    stacks = int(stack)
+    
+    for i in range(slices):
+        alpha = i * (2 * math.pi / slices)
+        alpha1 = (i + 1) * (2 * math.pi / slices)
+
+        for j in range(int(-(stacks / 2)), int(stacks/2)):
+            beta = j * (math.pi / stacks)
+            beta1 = (j + 1) * (math.pi /stacks)
+
+            x = radius * math.sin(alpha) * math.cos(beta) 
+            y = radius * math.sin(beta)
+            z = radius * math.cos(alpha) * math.cos(beta)
+
+            triangles.append((x,y,z))
+
+            x = radius * math.sin(alpha1) * math.cos(beta) 
+            y = radius * math.sin(beta)
+            z = radius * math.cos(alpha1) * math.cos(beta)
+
+            triangles.append((x,y,z))
+
+            x = radius * math.sin(alpha) * math.cos(beta1) 
+            y = radius * math.sin(beta1)
+            z = radius * math.cos(alpha) * math.cos(beta1)
+
+            triangles.append((x,y,z))
+
+            x = radius * math.sin(alpha1) * math.cos(beta) 
+            y = radius * math.sin(beta)
+            z = radius * math.cos(alpha1) * math.cos(beta)
+
+            triangles.append((x,y,z))
+
+            x = radius * math.sin(alpha1) * math.cos(beta1) 
+            y = radius * math.sin(beta1)
+            z = radius * math.cos(alpha1) * math.cos(beta1)
+
+            triangles.append((x,y,z))
+
+            x = radius * math.sin(alpha) * math.cos(beta1) 
+            y = radius * math.sin(beta1)
+            z = radius * math.cos(alpha) * math.cos(beta1)
+
+            triangles.append((x,y,z))
+
+    with open(outputFile, 'w') as f:
+        for i, l in enumerate(triangles):
+            f.write(' '.join(str(x) for x in l) + '\n')
+            if (i+1) % 3 == 0:
+                f.write('\n')
+
+def generateBox(size, divisions, outputFile):
+    step = size / divisions
+    triangles = []
+
+    for i in range(int(divisions)):
+        for j in range(int(divisions)):
+            
+            x1 = -size/2 + i*step
+            z1 = -size/2 + j*step
+            y1 = size/2
+
+            x2 = -size/2 + (i+1)*step
+            z2 = -size/2 + j*step
+            y2 = size/2
+
+            x3 = -size/2 + i*step
+            z3 = -size/2 + (j+1)*step
+            y3 = size/2
+
+            x4 = -size/2 + (i+1)*step
+            z4 = -size/2 + (j+1)*step
+            y4 = size/2
+
+            triangles.append([x3, y3, z3])
+            triangles.append([x2, y2, z2])
+            triangles.append([x1, y1, z1])
+                
+            triangles.append([x3, y3, z3])
+            triangles.append([x4, y4, z4])
+            triangles.append([x2, y2, z2])
+
+            triangles.append([x3, -y3, z3])
+            triangles.append([x1, -y1, z1])
+            triangles.append([x2, -y2, z2])
+            
+            triangles.append([x3, -y3, z3])
+            triangles.append([x2, -y2, z2])
+            triangles.append([x4, -y4, z4])
+              
+
+    for i in range(int(divisions)):
+        for j in range(int(divisions)):
+            
+            x1 = -size/2 + i*step
+            y1 = -size/2 + j*step
+            z1 = size/2
+
+            x2 = -size/2 + (i+1)*step
+            y2 = -size/2 + j*step
+            z2 = size/2
+
+            x3 = -size/2 + i*step
+            y3 = -size/2 + (j+1)*step
+            z3 = size/2
+
+            x4 = -size/2 + (i+1)*step
+            y4 = -size/2 + (j+1)*step
+            z4 = size/2
+
+            triangles.append([x3, y3, -z3])
+            triangles.append([x2, y2, -z2])
+            triangles.append([x1, y1, -z1])
+                
+            triangles.append([x3, y3, -z3])
+            triangles.append([x4, y4, -z4])
+            triangles.append([x2, y2, -z2])
+
+            triangles.append([x3, y3, z3])
+            triangles.append([x1, y1, z1])
+            triangles.append([x2, y2, z2])
+            
+            triangles.append([x3, y3, z3])
+            triangles.append([x2, y2, z2])
+            triangles.append([x4, y4, z4]) 
+
+
+    for i in range(int(divisions)):
+        for j in range(int(divisions)):
+            
+            z1 = -size/2 + i*step
+            y1 = -size/2 + j*step
+            x1 = size/2
+
+            z2 = -size/2 + (i+1)*step
+            y2 = -size/2 + j*step
+            x2 = size/2
+
+            z3 = -size/2 + i*step
+            y3 = -size/2 + (j+1)*step
+            x3 = size/2
+
+            z4 = -size/2 + (i+1)*step
+            y4 = -size/2 + (j+1)*step
+            x4 = size/2
+
+            triangles.append([-x3, y3, z3])
+            triangles.append([-x1, y1, z1])
+            triangles.append([-x2, y2, z2])
+                
+            triangles.append([-x3, y3, z3])
+            triangles.append([-x2, y2, z2])
+            triangles.append([-x4, y4, z4])
+
+            triangles.append([x3, y3, -z3])
+            triangles.append([x1, y1, -z1])
+            triangles.append([x2, y2, -z2])
+                           
+            triangles.append([x3, y3, -z3])
+            triangles.append([x2, y2, -z2])
+            triangles.append([x4, y4, -z4])
+    
+    with open(outputFile, 'w') as f:
+        for i, l in enumerate(triangles):
+            f.write(' '.join(str(x) for x in l) + '\n')
+            if (i+1) % 3 == 0:
+                f.write('\n')
+
+def generateCone(radius, height, slice, stack, outputFile):
+    triangles = []
+
+    slices = int(slice)
+    stacks = int(stack)
+
+    for j in range(stacks):
+        for i in range(slices): 
+            angle = 2 * math.pi / slices
+            z = radius * math.cos(i * angle)
+            x = radius * math.sin(i * angle)
+            zz = radius * math.cos((i + 1) * angle)
+            xx = radius * math.sin((i + 1) * angle)
+
+            if j == 0:
+                a = xx * ((stacks - j) / stacks)
+                b = height * j / stacks
+                c = zz * ((stacks - j) / stacks)
+
+                triangles.append((a, b, c))
+
+                a = x * ((stacks - j) / stacks)
+                b = height * j / stacks
+                c = z * ((stacks - j) / stacks)
+
+                triangles.append((a, b, c))
+                
+                a = 0
+                b = height * j / stacks
+                c = 0
+
+                triangles.append((a, b, c))
+
+            angle2 = math.pi / slices
+
+            if (j < stacks - 1):
+                a = x * ((stacks - j) / stacks)
+                b = height * j / stacks
+                c = z * ((stacks - j) / stacks)
+
+                triangles.append((a, b, c))
+                
+                a = xx * ((stacks - j) / stacks)
+                b = height * j / stacks
+                c = zz * ((stacks - j) / stacks)
+
+                triangles.append((a, b, c))
+                
+                a = x * ((stacks - j - 1) / stacks)
+                b = height * (j + 1) / stacks
+                c = z * ((stacks - j - 1) / stacks)
+
+                triangles.append((a, b, c))
+                
+                a = xx * ((stacks - j) / stacks)
+                b = height * j / stacks
+                c = zz * ((stacks - j) / stacks)
+
+                triangles.append((a, b, c))
+                
+                a = xx * ((stacks - j - 1) / stacks)
+                b = height * (j + 1) / stacks
+                c = zz * ((stacks - j - 1) / stacks)
+
+                triangles.append((a, b, c))
+
+                a = x * ((stacks - j - 1) / stacks)
+                b = height * (j + 1) / stacks
+                c = z * ((stacks - j - 1) / stacks)
+
+                triangles.append((a, b, c))
+
+            else:
+                a = x * ((stacks - j) / stacks)
+                b = height * j / stacks
+                c = z * ((stacks - j) / stacks)
+
+                triangles.append((a, b, c))
+
+                a = xx * ((stacks - j) / stacks)
+                b = height * j / stacks
+                c = zz * ((stacks - j) / stacks)
+
+                triangles.append((a, b, c))
+
+                a = 0
+                b = height
+                c = 0
+
+                triangles.append((a, b, c))
+
+    with open(outputFile, 'w') as f:
+        for i, l in enumerate(triangles):
+            f.write(' '.join(str(x) for x in l) + '\n')
+            if (i+1) % 3 == 0:
+                f.write('\n')
+
+def generatePlane(size, divisions, outputFile):
+    step = size / divisions
+    triangles = []
+    
+    for i in range(int(divisions)):
+        for j in range(int(divisions)):
+            x1 = -size/2 + i*step
+            z1 = -size/2 + j*step
+            y1 = 0
+
+            x2 = -size/2 + (i+1)*step
+            z2 = -size/2 + j*step
+            y2 = 0
+
+            x3 = -size/2 + i*step
+            z3 = -size/2 + (j+1)*step
+            y3 = 0
+
+            x4 = -size/2 + (i+1)*step
+            z4 = -size/2 + (j+1)*step
+            y4 = 0
+
+            triangles.append([x4, y4, z4])
+            triangles.append([x2, y2, z2])
+            triangles.append([x1, y1, z1])
+                
+            triangles.append([x3, y3, z3])
+            triangles.append([x4, y4, z4])
+            triangles.append([x1, y1, z1])
+
+
+            triangles.append([x3, y3, z3])
+            triangles.append([x1, y1, z1])
+            triangles.append([x2, y2, z2])            
+              
+            triangles.append([x3, y3, z3])
+            triangles.append([x2, y2, z2])
+            triangles.append([x4, y4, z4])          
+
+    with open(outputFile, 'w') as f:
+        for i, l in enumerate(triangles):
+            f.write(' '.join(str(x) for x in l) + '\n')
+            if (i+1) % 3 == 0:
+                f.write('\n')
+
+def generateCylinder(radius, height, side, stack, outputFile):
+   
+    triangles = []
+    sides = int(side)
+    stacks = int(stack)
+
+    for i in range(sides):
+        angle = i * (2 * math.pi / sides)
+        value = (i + 1) * (2 * math.pi / sides)
+
+        # Top
+        x = radius * math.sin(angle)
+        y = height 
+        z = radius * math.cos(angle)
+
+        triangles.append((x,y,z))
+ 
+        x = radius * math.sin(value)
+        y = height 
+        z = radius * math.cos(value)
+        
+        triangles.append((x,y,z))       
+
+        x = 0
+        y = height 
+        z = 0
+
+        triangles.append((x,y,z))
+
+        for j in range(stacks):
+            
+            # Body
+            x = radius * math.sin(angle)
+            y = height  * (j / stacks)
+            z = radius * math.cos(angle)
+
+            triangles.append((x,y,z))
+    
+            x = radius * math.sin(value)
+            y = height  * (j / stacks)
+            z = radius * math.cos(value)
+            
+            triangles.append((x,y,z))       
+
+            x = radius * math.sin(angle)
+            y = height  * ((j + 1) / stacks)
+            z = radius * math.cos(angle)
+
+            triangles.append((x,y,z))
+
+
+            x = radius * math.sin(angle)
+            y = height * ((j + 1) / stacks)
+            z = radius * math.cos(angle)
+
+            triangles.append((x,y,z))
+    
+            x = radius * math.sin(value)
+            y = height * (j / stacks)
+            z = radius * math.cos(value)
+            
+            triangles.append((x,y,z))       
+
+            x = radius * math.sin(value)
+            y = height * ((j + 1) / stacks)
+            z = radius * math.cos(value)
+
+            triangles.append((x,y,z))
+        
+        # Bottom
+        x = radius * math.sin(angle)
+        y = 0 
+        z = radius * math.cos(angle)
+
+        triangles.append((x,y,z))
+ 
+        x = 0
+        y = 0
+        z = 0
+
+        triangles.append((x,y,z))
+        
+        x = radius * math.sin(value)
+        y = 0 
+        z = radius * math.cos(value)
+        
+        triangles.append((x,y,z))       
+    
+    with open(outputFile, 'w') as f:
+        for i, l in enumerate(triangles):
+            f.write(' '.join(str(x) for x in l) + '\n')
+            if (i+1) % 3 == 0:
+                f.write('\n')
+
+shapes = {
+    'sphere': {
+        'func': generateSphere,
+        'args': ['radius', 'slices', 'stack', 'outputFile']
+    },
+    'box': {
+        'func': generateBox,
+        'args': ['size', 'divisions' 'outputFile']
+    },
+    'cone': {
+        'func': generateCone,
+        'args': ['radius', 'height', 'slices', 'stack', 'outputFile']
+    },
+    'plane': {
+        'func': generatePlane,
+        'args': ['x', 'z', 'outputFile']
+    },
+    'cylinder':{
+        'func': generateCylinder,
+        'args': ['radius', 'height', 'sides', 'stacks', 'outputFile']
+    }
+}
+
+if len(sys.argv) < 4:
+    print('Usage: generator.py <shape> <arg1> <arg2> ... <outputFile>')
+    sys.exit(1)
+
+shapeName = sys.argv[1]
+if shapeName not in shapes:
+    print(f'Invalid shape name: {shapeName}')
+    sys.exit(1)
+
+args = sys.argv[2:-1]
+
+outputFile = sys.argv[-1]
+
+shapeFunc = shapes[shapeName]['func']
+shapeArgs = [float(arg) for arg in args] + [outputFile]
+shapeFunc(*shapeArgs)
+
+print(f'Successfully generated {shapeName} in file {outputFile}')
+
---
+++ Checkpoint 2/src/main.cpp
@@ -0,0 +1,286 @@
+#define _USE_MATH_DEFINES
+#include <stdio.h>
+#include <math.h>
+#include <iostream>
+#include <string>
+#include <io.h>
+#include <direct.h>
+
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+#include "rapidxml.hpp"
+#include "rapidxml_utils.hpp"
+#include "rapidxml_print.hpp"
+
+using namespace rapidxml;
+
+float alpha = 0.0f;
+float beta = 0.0f;
+
+int width = 0;
+int height = 0;
+
+float fov = 0.0f;
+float near = 0.0f;
+float far = 0.0f;
+
+std::vector<float> cameraValues;
+std::vector<std::vector<float>> modelsCoords;
+
+std::vector<float> translateValues;
+std::vector<float> scaleValues;
+float rotateAngle = 0.0f;
+float rotateX = 0.0f;
+float rotateY = 0.0f;
+float rotateZ = 0.0f;
+
+void changeSize(int w, int h) {
+
+	// Prevent a divide by zero, when window is too short
+	// (you cant make a window with zero width).
+	if(h == 0)
+		h = 1;
+
+	// compute window's aspect ratio 
+	float ratio = w * 1.0 / h;
+
+	// Set the projection matrix as current
+	glMatrixMode(GL_PROJECTION);
+	// Load Identity Matrix
+	glLoadIdentity();
+	
+	// Set the viewport to be the entire window
+	glViewport(0, 0, width, height);
+
+	gluPerspective(fov, width / height, near, far);
+
+	// return to the model view matrix mode
+	glMatrixMode(GL_MODELVIEW);
+}
+
+
+void drawShape() {
+	for (int i = 0; i < modelsCoords.size(); i++) {
+		std::vector<float> coords = modelsCoords[i];
+
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		// Draw the model
+		glBegin(GL_TRIANGLES);
+		for (int j = 0; j < coords.size(); j += 9) {
+			glVertex3f(coords[j], coords[j + 1], coords[j + 2]);
+			glVertex3f(coords[j + 3], coords[j + 4], coords[j + 5]);
+			glVertex3f(coords[j + 6], coords[j + 7], coords[j + 8]);
+		}
+		glEnd();
+	}
+}
+
+
+void transform() {
+	glPushMatrix();
+	glTranslatef(translateValues[0], translateValues[1], translateValues[2]);
+	printf("Translation -> X: %d  Y: %d  Z: %d", translateValues[0], translateValues[1], translateValues[2]);
+	glRotatef(rotateAngle, rotateX, rotateY, rotateZ);
+	glScalef(scaleValues[0], scaleValues[1], scaleValues[2]);
+	glPopMatrix();
+}
+
+
+void renderScene() {
+
+	float x = cameraValues[0];
+	float y = cameraValues[1];
+	float z = cameraValues[2];
+	float lookAtX = cameraValues[3];
+	float lookAtY = cameraValues[4];
+	float lookAtZ = cameraValues[5];
+	float upX = cameraValues[6];
+	float upY = cameraValues[7];
+	float upZ = cameraValues[8];
+
+	// clear buffers
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	// set the camera
+	glLoadIdentity();
+	
+	float camX = cos(beta) * sin(alpha);
+	float camZ = cos(beta) * cos(alpha);
+	gluLookAt(x, y, z,
+		      lookAtX,lookAtY,lookAtZ,
+			  upX,upY,upZ);
+
+	glBegin(GL_LINES);
+		// X axis in red
+		glColor3f(1.0f, 0.0f, 0.0f);
+		glVertex3f(
+			-100.0f, 0.0f, 0.0f);
+		glVertex3f(100.0f, 0.0f, 0.0f);
+		// Y Axis in Green
+		glColor3f(0.0f, 1.0f, 0.0f);
+		glVertex3f(0.0f,
+			-100.0f, 0.0f);
+		glVertex3f(0.0f, 100.0f, 0.0f);
+		// Z Axis in Blue
+		glColor3f(0.0f, 0.0f, 1.0f);
+		glVertex3f(0.0f, 0.0f,
+			-100.0f);
+		glVertex3f(0.0f, 0.0f, 100.0f);
+	glEnd();
+
+	glColor3f(1.0f, 1.0f, 1.0f);
+
+	transform();
+	drawShape();
+
+	// End of frame
+	glutSwapBuffers();
+}
+
+
+void processKeys(unsigned char key, int xx, int yy) {
+	switch (key) {
+	case 'a':
+	case 'A':
+		alpha -= 0.1f;
+		break;
+	case 'd':
+	case 'D':
+		alpha += 0.1f;
+		break;
+	case 'w':
+	case 'W':
+		if(beta <= 1.5f)
+			beta += 0.1f;
+		break;
+	case 's':
+	case 'S':
+		if(beta >= -1.5f)
+			beta -= 0.1f;
+		break;
+	default:
+		break;
+	}
+	glutPostRedisplay();
+
+}
+
+
+void loadXML() {
+
+	// Load the XML file
+	file<> xmlFile("../src/configuration.xml");
+
+	// Parse the XML file
+	xml_document<> doc;
+	doc.parse<0>(xmlFile.data());
+
+	// Get the root node
+	xml_node<>* rootNode = doc.first_node("world");
+
+	// Get the window node and its attributes
+	xml_node<>* windowNode = rootNode->first_node("window");
+	width = std::stoi(windowNode->first_attribute("width")->value());
+	height = std::stoi(windowNode->first_attribute("height")->value());
+
+	// Get the camera node and its children
+	xml_node<>* cameraNode = rootNode->first_node("camera");
+	xml_node<>* positionNode = cameraNode->first_node("position");
+	xml_node<>* lookAtNode = cameraNode->first_node("lookAt");
+	xml_node<>* upNode = cameraNode->first_node("up");
+	xml_node<>* projectionNode = cameraNode->first_node("projection");
+
+	cameraValues.push_back(std::stof(positionNode->first_attribute("x")->value()));
+	cameraValues.push_back(std::stof(positionNode->first_attribute("y")->value()));
+	cameraValues.push_back(std::stof(positionNode->first_attribute("z")->value()));
+	std::cout << "Camera Values: " << "X: " << std::stof(positionNode->first_attribute("x")->value()) << " Y: " << std::stof(positionNode->first_attribute("y")->value()) << " Z: " << std::stof(positionNode->first_attribute("z")->value()) << std::endl;
+	cameraValues.push_back(std::stof(lookAtNode->first_attribute("x")->value()));
+	cameraValues.push_back(std::stof(lookAtNode->first_attribute("y")->value()));
+	cameraValues.push_back(std::stof(lookAtNode->first_attribute("z")->value()));
+	cameraValues.push_back(std::stof(upNode->first_attribute("x")->value()));
+	cameraValues.push_back(std::stof(upNode->first_attribute("y")->value()));
+	cameraValues.push_back(std::stof(upNode->first_attribute("z")->value()));
+	fov = std::stof(projectionNode->first_attribute("fov")->value());
+	near = std::stof(projectionNode->first_attribute("near")->value());
+	far = std::stof(projectionNode->first_attribute("far")->value());
+	std::cout << "Camera Projection: " << "FOV: " << fov << " Near: " << near << " Far: " << far << std::endl;
+
+	// Get the group node and its child nodes
+	xml_node<>* groupNode = rootNode->first_node("group");
+
+	xml_node<>* transformNode = groupNode->first_node("transform");
+	xml_node<>* translateNode = transformNode->first_node("translate");
+	xml_node<>* rotateNode = transformNode->first_node("rotate");
+	xml_node<>* scaleNode = transformNode->first_node("scale");
+
+	translateValues.push_back(std::stof(translateNode->first_attribute("x")->value()));
+	translateValues.push_back(std::stof(translateNode->first_attribute("y")->value()));
+	translateValues.push_back(std::stof(translateNode->first_attribute("z")->value()));
+	rotateAngle = std::stof(rotateNode->first_attribute("angle")->value());
+	rotateX = std::stof(rotateNode->first_attribute("x")->value());
+	rotateY = std::stof(rotateNode->first_attribute("y")->value());
+	rotateZ = std::stof(rotateNode->first_attribute("z")->value());
+	scaleValues.push_back(std::stof(scaleNode->first_attribute("x")->value()));
+	scaleValues.push_back(std::stof(scaleNode->first_attribute("y")->value()));
+	scaleValues.push_back(std::stof(scaleNode->first_attribute("z")->value()));
+
+	xml_node<>* modelsNode = groupNode->first_node("models");
+	xml_node<>* modelNode = modelsNode->first_node("model");
+
+	while (modelNode != NULL)
+	{
+		char cwd[1024];
+
+		std::string file = modelNode->first_attribute("file")->value();
+
+		std::vector<float> coords;
+		float x, y, z;
+		std::string file_path = "../src/" + file;
+		std::cout << "Loading model from file: " << file_path << std::endl;
+
+		std::ifstream infile(file_path);
+
+		while (infile >> x >> y >> z) {
+			coords.push_back(x);
+			coords.push_back(y);
+			coords.push_back(z);
+		}
+
+		modelsCoords.push_back(coords);
+
+		modelNode = modelNode->next_sibling("model");
+	}
+}
+
+
+int main(int argc, char **argv) {
+
+	loadXML();
+
+// init GLUT and the window
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH|GLUT_DOUBLE|GLUT_RGBA);
+	glutInitWindowPosition(100,100);
+	glutInitWindowSize(width,height);
+	glutCreateWindow("grupo31");
+		
+// Required callback registry 
+	glutDisplayFunc(renderScene);
+	glutReshapeFunc(changeSize);
+	
+// Callback registration for keyboard processing
+	glutKeyboardFunc(processKeys);
+
+//  OpenGL settings
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_CULL_FACE);
+	
+// enter GLUT's main cycle
+	glutMainLoop();
+	
+	return 1;
+}
