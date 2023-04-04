6e1c01431a1938a987a57aea372df97e13a8e965
Hugo Novais <a96267@alunos.uminho.pt>
2023-04-04 16:33:23 +0100

    código do checkpoint 2

--- Checkpoint 2/src/main.cpp
+++ Checkpoint 2/src/main.cpp
@@ -26,22 +26,6 @@ XML_Struct estrutura;
 float alpha = 0.0f;
 float beta = 0.0f;
 
-int width = 0;
-int height = 0;
-
-float fov = 0.0f;
-float near = 0.0f;
-float far = 0.0f;
-
-std::vector<float> cameraValues;
-std::vector<std::vector<float>> modelsCoords;
-
-std::vector<std::vector<float>> translateValues;
-std::vector<std::vector<float>> rotateValues;
-std::vector<std::vector<float>> scaleValues;
-
-std::vector<int> xmlValues;
-
 void changeSize(int w, int h) {
 
 	// Prevent a divide by zero, when window is too short
@@ -58,30 +42,15 @@ void changeSize(int w, int h) {
 	glLoadIdentity();
 	
 	// Set the viewport to be the entire window
-	glViewport(0, 0, width, height);
+	glViewport(0, 0, estrutura.window.width, estrutura.window.height);
 
-	gluPerspective(fov, width / height, near, far);
+	gluPerspective(estrutura.camera.projection.fov, estrutura.window.width / estrutura.window.height, estrutura.camera.projection.near, estrutura.camera.projection.far);
 
 	// return to the model view matrix mode
 	glMatrixMode(GL_MODELVIEW);
 }
 
-
-void drawShape(int i) {
-	std::vector<float> coords = modelsCoords[i];
-
-	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
-	glBegin(GL_TRIANGLES);
-	for (int j = 0; j < coords.size(); j += 9) {
-		glVertex3f(coords[j], coords[j + 1], coords[j + 2]);
-		glVertex3f(coords[j + 3], coords[j + 4], coords[j + 5]);
-		glVertex3f(coords[j + 6], coords[j + 7], coords[j + 8]);
-	}
-	glEnd();
-}
-
-void drawShape2(Model model) {
+void drawShape(Model model) {
 	vector<float> coords = model.points;
 
 	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
@@ -96,68 +65,30 @@ void drawShape2(Model model) {
 }
 
 
-void readValues() {
-	int models = 0;
-	int transforms = 0;
-	int groups = 1;
-
-	//glPushMatrix();
-
-	for (int i = 0; i < xmlValues.size(); i++) {
-		int action = xmlValues[i];
-
-		if (action == 0) {
-			glPushMatrix();
-			groups += 1;
-		}
-
-		else if (action == 1) {
-			std::vector<float> tran = translateValues[transforms];/*
-			std::vector<float> rot = rotateValues[transforms];
-			std::vector<float> scl = scaleValues[transforms];
-			*/
-			glTranslatef(tran[0], tran[1], tran[2]);
-			//glRotatef(rot[0], rot[1], rot[2], rot[3]);
-			//glScalef(scl[0], scl[1], scl[2]);
-
-			transforms += 1;
-		}
-
-		else if (action == 2) {
-			drawShape(models);
-			models += 1;
-		}
-	}
-
-	for (int j = 0; j < groups; j++) {
-		glPopMatrix();
-	}
-}
-
 void transformation(Transform transform) {
 	if (transform.type=="translate"){
-		glTranslated(transform.x, transform.y, transform.z);
+		glTranslatef(transform.x, transform.y, transform.z);
 	}
 	else if (transform.type == "rotate") {
-		glRotated(transform.angle,transform.x, transform.y, transform.z);
+		glRotatef(transform.angle,transform.x, transform.y, transform.z);
 	}
 	else if (transform.type == "scale") {
-		glScaled(transform.x, transform.y, transform.z);
+		glScalef(transform.x, transform.y, transform.z);
 	}
 }
 
 void processgroup(Group group) {
 	glPushMatrix();
 
-	for (int i;i < group.transforms.size();i++) {
+	for (int i = 0; i < group.transforms.size(); i++) {
 		transformation(group.transforms[i]);
 	}
 
-	for (int i;i < group.models.size();i++) {
-		drawShape2(group.models[i]);
+	for (int i = 0; i < group.models.size(); i++) {
+		drawShape(group.models[i]);
 	}
 
-	for (int i;i < group.groups.size();i++) {
+	for (int i = 0; i < group.groups.size(); i++) {
 		processgroup(group.groups[i]);
 	}
 
@@ -165,16 +96,6 @@ void processgroup(Group group) {
 }
 
 void renderScene() {
-
-	float x = cameraValues[0];
-	float y = cameraValues[1];
-	float z = cameraValues[2];
-	float lookAtX = cameraValues[3];
-	float lookAtY = cameraValues[4];
-	float lookAtZ = cameraValues[5];
-	float upX = cameraValues[6];
-	float upY = cameraValues[7];
-	float upZ = cameraValues[8];
 
 	// clear buffers
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -184,11 +105,6 @@ void renderScene() {
 	
 	float camX = cos(beta) * sin(alpha);
 	float camZ = cos(beta) * cos(alpha);
-	/*
-	gluLookAt(x, y, z,
-		      lookAtX,lookAtY,lookAtZ,
-			  upX,upY,upZ);
-	*/
 
 	gluLookAt(estrutura.camera.position.x, estrutura.camera.position.y, estrutura.camera.position.z,
 		estrutura.camera.lookAt.x, estrutura.camera.lookAt.y, estrutura.camera.lookAt.z,
@@ -214,8 +130,8 @@ void renderScene() {
 
 	glColor3f(1.0f, 1.0f, 1.0f);
 
-	//processgroup(estrutura.group);
-	readValues();
+	processgroup(estrutura.group);
+	
 	// End of frame
 	glutSwapBuffers();
 }
@@ -248,6 +164,98 @@ void processKeys(unsigned char key, int xx, int yy) {
 }
 
 
+Group readGroup(Group group, xml_node<>* groupNode) {
+	xml_node<>* transformNode = groupNode->first_node("transform");
+	xml_node<>* modelsNode = groupNode->first_node("models");
+	xml_node<>* groupsNode = groupNode->first_node("group");
+
+	if (transformNode != NULL) {
+		for (xml_node<>* childNode = transformNode->first_node(); childNode != NULL; childNode = childNode->next_sibling()) {
+			string nodeName = childNode->name();
+
+			if (nodeName == "translate") {
+				Transform translate;
+
+				translate.type = "translate";
+				translate.x = stof(childNode->first_attribute("x")->value());
+				translate.y = stof(childNode->first_attribute("y")->value());
+				translate.z = stof(childNode->first_attribute("z")->value());
+
+				printf("Tranlation: X: %.1f  Y: %.1f  Z: %.1f\n", translate.x, translate.y, translate.z);
+
+				group.transforms.push_back(translate);
+			}
+
+			if (nodeName == "rotate") {
+				Transform rotate;
+
+				rotate.type = "rotate";
+				rotate.angle = stof(childNode->first_attribute("angle")->value());
+				rotate.x = stof(childNode->first_attribute("x")->value());
+				rotate.y = stof(childNode->first_attribute("y")->value());
+				rotate.z = stof(childNode->first_attribute("z")->value());
+
+				printf("Rotation: Angle: %.1f X: %.1f  Y: %.1f  Z: %.1f\n", rotate.angle, rotate.x, rotate.y, rotate.z);
+
+				group.transforms.push_back(rotate);
+			}
+
+			if (nodeName == "scale") {
+				Transform scale;
+
+				scale.type = "scale";
+				scale.x = stof(childNode->first_attribute("x")->value());
+				scale.y = stof(childNode->first_attribute("y")->value());
+				scale.z = stof(childNode->first_attribute("z")->value());
+
+				printf("Scale: X: %.1f  Y: %.1f  Z: %.1f\n", scale.x, scale.y, scale.z);
+
+				group.transforms.push_back(scale);
+			}
+		}
+	}
+
+	if (modelsNode != NULL) {
+		xml_node<>* modelNode = modelsNode->first_node("model");
+
+		while (modelNode != NULL)
+		{
+			char cwd[1024];
+
+			string file = modelNode->first_attribute("file")->value();
+
+			Model model;
+
+			float x, y, z;
+			string file_path = "../src/" + file;
+			cout << "Loading model from file: " << file_path << "\n" << std::endl;
+
+			ifstream infile(file_path);
+
+			while (infile >> x >> y >> z) {
+				model.points.push_back(x);
+				model.points.push_back(y);
+				model.points.push_back(z);
+			}
+
+			group.models.push_back(model);
+
+			modelNode = modelNode->next_sibling("model");
+		}
+	}
+
+	if (groupsNode != NULL) {
+		while (groupsNode != NULL) {
+			Group newGroup;
+			group.groups.push_back(readGroup(newGroup, groupsNode));
+			groupsNode = groupsNode->next_sibling("group");
+		}
+	}
+
+	return group;
+}
+
+
 void loadXML(XML_Struct& estrutura) {
 
 	// Load the XML file
@@ -262,9 +270,6 @@ void loadXML(XML_Struct& estrutura) {
 
 	// Get the window node and its attributes
 	xml_node<>* windowNode = rootNode->first_node("window");
-	width = std::stoi(windowNode->first_attribute("width")->value());
-	height = std::stoi(windowNode->first_attribute("height")->value());
-
 	estrutura.window.width= stoi(windowNode->first_attribute("width")->value());
 	estrutura.window.height = stoi(windowNode->first_attribute("height")->value());
 
@@ -275,173 +280,32 @@ void loadXML(XML_Struct& estrutura) {
 	xml_node<>* upNode = cameraNode->first_node("up");
 	xml_node<>* projectionNode = cameraNode->first_node("projection");
 
-	cameraValues.push_back(std::stof(positionNode->first_attribute("x")->value()));
-	cameraValues.push_back(std::stof(positionNode->first_attribute("y")->value()));
-	cameraValues.push_back(std::stof(positionNode->first_attribute("z")->value()));
 	std::cout << "Camera Values: " << "X: " << std::stof(positionNode->first_attribute("x")->value()) << " Y: " << std::stof(positionNode->first_attribute("y")->value()) << " Z: " << std::stof(positionNode->first_attribute("z")->value()) << std::endl;
 	estrutura.camera.position.x = stof(positionNode->first_attribute("x")->value());
 	estrutura.camera.position.y = stof(positionNode->first_attribute("y")->value());
 	estrutura.camera.position.z = stof(positionNode->first_attribute("z")->value());
 
-	cameraValues.push_back(std::stof(lookAtNode->first_attribute("x")->value()));
-	cameraValues.push_back(std::stof(lookAtNode->first_attribute("y")->value()));
-	cameraValues.push_back(std::stof(lookAtNode->first_attribute("z")->value()));
 	estrutura.camera.lookAt.x = stof(lookAtNode->first_attribute("x")->value());
 	estrutura.camera.lookAt.y = stof(lookAtNode->first_attribute("y")->value());
 	estrutura.camera.lookAt.z = stof(lookAtNode->first_attribute("z")->value());
+	printf("Look At: X: %.1f  Y: %.1f  Z: %.1f\n", estrutura.camera.lookAt.x, estrutura.camera.lookAt.y, estrutura.camera.lookAt.z);
 
-	cameraValues.push_back(std::stof(upNode->first_attribute("x")->value()));
-	cameraValues.push_back(std::stof(upNode->first_attribute("y")->value()));
-	cameraValues.push_back(std::stof(upNode->first_attribute("z")->value()));
 	estrutura.camera.up.x = stof(upNode->first_attribute("x")->value());
 	estrutura.camera.up.y = stof(upNode->first_attribute("y")->value());
 	estrutura.camera.up.z = stof(upNode->first_attribute("z")->value());
+	printf("Up: X: %.1f  Y: %.1f  Z: %.1f\n", estrutura.camera.up.x, estrutura.camera.up.y, estrutura.camera.up.z);
 
-	fov = std::stof(projectionNode->first_attribute("fov")->value());
-	near = std::stof(projectionNode->first_attribute("near")->value());
-	far = std::stof(projectionNode->first_attribute("far")->value());
-	std::cout << "Camera Projection: " << "FOV: " << fov << " Near: " << near << " Far: " << far << std::endl;
-	estrutura.camera.projection.far= stof(projectionNode->first_attribute("fov")->value());;
-	estrutura.camera.projection.fov= stof(projectionNode->first_attribute("far")->value());;
+	estrutura.camera.projection.far= stof(projectionNode->first_attribute("far")->value());;
+	estrutura.camera.projection.fov= stof(projectionNode->first_attribute("fov")->value());;
 	estrutura.camera.projection.near=stof(projectionNode->first_attribute("near")->value());
+	printf("Projection: FOV: %.1f  near: %.1f  far: %.1f\n\n", estrutura.camera.projection.fov, estrutura.camera.projection.near, estrutura.camera.projection.far);
 
 	// Get the group node and its child nodes
 
 	xml_node<>* groupNode = rootNode->first_node("group");
 
-	while (groupNode != NULL) {
-		xmlValues.push_back(0);
-
-		xml_node<>* transformNode = groupNode->first_node("transform");
-		
-		if (transformNode != NULL) {
-			xml_node<>* translateNode = transformNode->first_node("translate");
-			xml_node<>* rotateNode = transformNode->first_node("rotate");
-			xml_node<>* scaleNode = transformNode->first_node("scale");
-
-			float a = 0.0f;
-			float x = 0.0f;
-			float y = 0.0f;
-			float z = 0.0f;
-
-			std::vector<float> coords;
-
-			if (translateNode != NULL) {
-				x = std::stof(translateNode->first_attribute("x")->value());
-				y = std::stof(translateNode->first_attribute("y")->value());
-				z = std::stof(translateNode->first_attribute("z")->value());
-				std::cout << "Translation: " << "X: " << x << " Y: " << y << " Z: " << z << std::endl;
-				coords.push_back(x);
-				coords.push_back(y);
-				coords.push_back(z);
-
-				translateValues.push_back(coords);
-			}
-			else {
-				x = 0;
-				y = 0;
-				z = 0;
-
-				coords.push_back(x);
-				coords.push_back(y);
-				coords.push_back(z);
-
-				translateValues.push_back(coords);
-			}
-
-			std::vector<float> rt;
-
-			if (rotateNode != NULL) {
-				a = std::stof(rotateNode->first_attribute("angle")->value());
-				x = std::stof(rotateNode->first_attribute("x")->value());
-				y = std::stof(rotateNode->first_attribute("y")->value());
-				z = std::stof(rotateNode->first_attribute("z")->value());
-
-				rt.push_back(a);
-				rt.push_back(x);
-				rt.push_back(y);
-				rt.push_back(z);
-
-				rotateValues.push_back(rt);
-			}
-			else {
-				a = 0.0f;
-				x = 0.0f;
-				y = 0.0f;
-				z = 0.0f;
-
-				rt.push_back(a);
-				rt.push_back(x);
-				rt.push_back(y);
-				rt.push_back(z);
-
-				rotateValues.push_back(rt);
-			}
-
-			std::vector<float> sc;
-
-			if (scaleNode != NULL) {
-				x = std::stof(scaleNode->first_attribute("x")->value());
-				y = std::stof(scaleNode->first_attribute("y")->value());
-				z = std::stof(scaleNode->first_attribute("z")->value());
-
-				sc.push_back(x);
-				sc.push_back(y);
-				sc.push_back(z);
-
-				scaleValues.push_back(sc);
-			}
-			else {
-				x = 0.0f;
-				y = 0.0f;
-				z = 0.0f;
-
-				sc.push_back(x);
-				sc.push_back(y);
-				sc.push_back(z);
-
-				scaleValues.push_back(sc);
-			}
-			
-			xmlValues.push_back(1);
-		}
-
-		xml_node<>* modelsNode = groupNode->first_node("models");
-		
-		if (modelsNode != NULL) {
-			xml_node<>* modelNode = modelsNode->first_node("model");
-
-			while (modelNode != NULL)
-			{
-				char cwd[1024];
-
-				std::string file = modelNode->first_attribute("file")->value();
-
-				std::vector<float> coords;
-				float x, y, z;
-				std::string file_path = "../src/" + file;
-				std::cout << "Loading model from file: " << file_path << std::endl;
-
-				std::ifstream infile(file_path);
-
-				while (infile >> x >> y >> z) {
-					coords.push_back(x);
-					coords.push_back(y);
-					coords.push_back(z);
-				}
-
-				modelsCoords.push_back(coords);
-
-				modelNode = modelNode->next_sibling("model");
-			}
-
-			xmlValues.push_back(2);
-		}
-
-		xml_node<>* nextNode = groupNode->first_node("group");
-
-		groupNode = nextNode;
-	}
+	if (groupNode != NULL) 
+		estrutura.group = readGroup(estrutura.group, groupNode);
 }
 
 int main(int argc, char **argv) {
@@ -452,7 +316,7 @@ int main(int argc, char **argv) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DEPTH|GLUT_DOUBLE|GLUT_RGBA);
 	glutInitWindowPosition(100,100);
-	glutInitWindowSize(width,height);
+	glutInitWindowSize(estrutura.window.width, estrutura.window.height);
 	glutCreateWindow("grupo31");
 		
 // Required callback registry 
