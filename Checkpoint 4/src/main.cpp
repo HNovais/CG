ef31da7e837a95de60cb00a082b0836eda1f117f
Rui8338 <pedroguisesilva@gmail.com>
2023-06-03 16:05:46 +0100

    xml

--- Checkpoint 4/src/main.cpp
+++ Checkpoint 4/src/main.cpp
@@ -858,7 +858,7 @@ vector<Lights> readLights(xml_node<>* lightsNode) {
 void loadXML() {
 
 	// Load the XML file
-	file<> xmlFile("../xml/test.xml");
+	file<> xmlFile("../xml/configuration.xml");
 
 	// Parse the XML file
 	xml_document<> doc;
